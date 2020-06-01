f67a48e787f74a8ad8099dcde8d4c88b65ceed68
Nicolas <nico_cp05@hotmail.com>
2020-06-01 17:52:16 -0300

    Primer Commit Angular-simple-CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Employee} from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  
+  
+  employeeArray: Employee[] = [
+    {id: 1, name: "Ryan", country: "United State" },
+    {id: 2, name: "Mark", country: "Ireland" },
+    {id: 3, name: "Patrick", country: "France"}
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+    if(this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  delete() {
+    if(confirm("Are you sure you want to delete it?")) {
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee()
+    }
+  }
 }
